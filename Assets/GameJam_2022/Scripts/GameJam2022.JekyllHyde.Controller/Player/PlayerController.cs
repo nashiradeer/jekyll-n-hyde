49f197d6ecea65fcc85d6f771a45186a87711bce
Nashira <velocicraft2088@gmail.com>
2022-01-29 20:04:11 -0300

    Readicionar mudanças do PlayerController

--- Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Controller/Player/PlayerController.cs
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Controller/Player/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using GameJam2022.JekyllHyde.Domain.Interface;
 using UnityEngine;
 
@@ -10,6 +9,7 @@ namespace GameJam2022.JekyllHyde.Controller.Player
         
         private IPlayer Player { get; set; }
         private float Speed = 2.0f;
+        private Collider2D InteractiveObject = null;
 
         public void Init(IPlayer player)
         {
@@ -26,6 +26,16 @@ namespace GameJam2022.JekyllHyde.Controller.Player
 
             var move = new Vector3(axis, 0);
             transform.position += move * (Speed * Time.deltaTime);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (InteractiveObject == null && collision.tag == "Interactable") InteractiveObject = collision;
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (InteractiveObject == collision) InteractiveObject = null;
         }
     }
 }
