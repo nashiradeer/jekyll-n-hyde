d1bf0e3acbc80cd3705f113e6c8734c1303c124f
Nashira <velocicraft2088@gmail.com>
2022-01-29 19:46:42 -0300

    Esqueci de por juntos com o commit das interfaces

---
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Domain/Getable.cs
@@ -0,0 +1,19 @@
+﻿using GameJam2022.JekyllHyde.Domain.Interface;
+
+namespace GameJam2022.JekyllHyde.Domain
+{
+    public class Getable : IGetable
+    {
+        public int Identifier { get; private set; }
+
+        public Getable(int identifier)
+        {
+            Identifier = identifier;
+        }
+
+        public bool Interact()
+        {
+            return true;
+        }
+    }
+}
---
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Domain/Interactable.cs
@@ -0,0 +1,12 @@
+﻿using GameJam2022.JekyllHyde.Domain.Interface;
+
+namespace GameJam2022.JekyllHyde.Domain
+{
+    public class Interactable : IInteractable
+    {
+        public bool Interact()
+        {
+            return true;
+        }
+    }
+}
