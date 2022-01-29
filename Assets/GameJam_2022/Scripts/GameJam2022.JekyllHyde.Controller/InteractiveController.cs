103711bc2ba6257bee7e165fa0a622fdb70a1548
Nashira <velocicraft2088@gmail.com>
2022-01-29 19:42:05 -0300

    Criar um controlador para interativos

---
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Controller/InteractiveController.cs
@@ -0,0 +1,37 @@
+﻿using GameJam2022.JekyllHyde.Domain.Interface;
+using System;
+using UnityEngine;
+
+namespace GameJam2022.JekyllHyde.Controller
+{
+    public class InteractiveController : MonoBehaviour
+    {
+        public event Action<bool> OnHideStatusChange
+        {
+            add => _onHideStatusChange += value;
+            remove => _onHideStatusChange -= value;
+        }
+
+        private Action<bool> _onHideStatusChange { get; set; }
+
+        public IInteractable Interactable;
+        public IPlayer Player;
+
+        public void Interact()
+        {
+            if (Interactable.Interact())
+            {
+                if (Interactable is IGetable)
+                {
+                    Player.Items[(Interactable as IGetable).Identifier] = true;
+
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    if (Player.ToggleHide()) _onHideStatusChange?.Invoke(Player.Hidden);
+                }
+            }
+        }
+    }
+}
