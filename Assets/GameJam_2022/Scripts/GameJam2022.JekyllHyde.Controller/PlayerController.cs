f1b382d51b54de8e631bfd6fb659182dfd623252
Nashira <velocicraft2088@gmail.com>
2022-01-29 18:03:38 -0300

    Armazenar objetos interagiveis

--- Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Controller/PlayerController.cs
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Controller/PlayerController.cs
@@ -4,6 +4,16 @@ namespace GameJam2022.JekyllHyde.Controller
 {
     public class PlayerController : MonoBehaviour
     {
-        
+        private Collider2D InteractiveObject = null;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (InteractiveObject == null && collision.tag == "Interactable") InteractiveObject = collision;
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (InteractiveObject == collision) InteractiveObject = null;
+        }
     }
 }
