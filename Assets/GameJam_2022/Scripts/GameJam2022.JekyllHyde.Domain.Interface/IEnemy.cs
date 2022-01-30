0fb1a7057fd903956854a92b396ab62c751ffa91
Nashira <velocicraft2088@gmail.com>
2022-01-30 12:26:02 -0300

    Criar o domínio do inimigo

--- Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Domain.Interface/IEnemy.cs
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Domain.Interface/IEnemy.cs
@@ -2,6 +2,11 @@ namespace GameJam2022.JekyllHyde.Domain.Interface
 {
     public interface IEnemy
     {
-        
+        PlayerOrientation Orientation { get; }
+        int CurrentDirection { get; }
+        bool Chasing { get; }
+        float ChaseDistance { get; set; }
+
+        bool ChaseUpdate(bool playerHidden, float playerX, float distance);
     }
 }
--- Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Domain/Enemy.cs
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Domain/Enemy.cs
@@ -4,6 +4,48 @@ namespace GameJam2022.JekyllHyde.Domain
 {
     public class Enemy : IEnemy
     {
-        
+        public PlayerOrientation Orientation { get; private set; }
+        public int CurrentDirection { get; private set; }
+        public bool Chasing { get; private set; }
+        public float ChaseDistance { get; set; }
+
+        public Enemy(PlayerOrientation orientation, float chaseDistance)
+        {
+            Orientation = orientation;
+            CurrentDirection = (orientation == PlayerOrientation.Right) ? 1 : -1;
+            ChaseDistance = chaseDistance;
+            Chasing = false;
+        }
+
+        public bool ChaseUpdate(bool playerHidden, float playerX, float distance)
+        {
+            if (!Chasing && playerHidden)
+                return false;
+
+            if (Chasing && playerHidden)
+            {
+                Chasing = false;
+                return true;
+            }
+
+            if (playerX > 0 && CurrentDirection < 0)
+                return false;
+
+            if (playerX < 0 && CurrentDirection > 0)
+                return false;
+
+            if (distance < ChaseDistance && !Chasing)
+            {
+                Chasing = true;
+                return true;
+            }
+            else if (distance > ChaseDistance && Chasing)
+            {
+                Chasing = false;
+                return true;
+            }
+
+            return false;
+        }
     }
 }
