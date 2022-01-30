39aade4180103247e08fbbcb6937d67d9e1d7034
Nashira <velocicraft2088@gmail.com>
2022-01-30 14:24:48 -0300

    Criar o VFX para o inimigo

---
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Controller/Enemy/EnemyVfx.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using GameJam2022.JekyllHyde.Domain.Interface;
+using UnityEngine;
+
+namespace GameJam2022.JekyllHyde.Controller.Enemy
+{
+    public class EnemyVfx : MonoBehaviour
+    {
+        [field: SerializeField] private SpriteRenderer EnemySprite { get; set; }
+        [field: SerializeField] private Animator EnemyAnimator { get; set; }
+        public bool Moving = false;
+
+        public void Rotate(IEnemy enemy)
+        {
+            Vector3 endValue = enemy.Orientation == PlayerOrientation.Left ? new Vector3(0, 0, 0) : new Vector3(0, 180, 0);
+            EnemySprite.transform.DORotate(endValue, 0.5f);
+        }
+
+        private void Update()
+        {
+            if (Moving)
+            {
+                EnemyAnimator.speed = 1;
+                return;
+            }
+
+            EnemyAnimator.speed = 0;
+            EnemyAnimator.Rebind();
+        }
+    }
+}
