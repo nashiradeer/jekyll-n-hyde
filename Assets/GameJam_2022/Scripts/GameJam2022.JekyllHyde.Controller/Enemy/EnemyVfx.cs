65c9061aaf88d834e37b4c05b29f50d4005c90f4
Nashira <velocicraft2088@gmail.com>
2022-01-30 15:28:34 -0300

    Animação do Hyde acelerada no chase

--- Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Controller/Enemy/EnemyVfx.cs
+++ Assets/GameJam_2022/Scripts/GameJam2022.JekyllHyde.Controller/Enemy/EnemyVfx.cs
@@ -10,17 +10,27 @@ namespace GameJam2022.JekyllHyde.Controller.Enemy
         [field: SerializeField] private Animator EnemyAnimator { get; set; }
         public bool Moving = false;
 
+        private float AnimatorSpeed = 1f;
+
         public void Rotate(IEnemy enemy)
         {
             Vector3 endValue = enemy.Orientation == PlayerOrientation.Left ? new Vector3(0, 0, 0) : new Vector3(0, 180, 0);
             EnemySprite.transform.DORotate(endValue, 0.5f);
         }
 
+        public void ChaseAnimation(bool isChasing)
+        {
+            if (isChasing)
+                AnimatorSpeed = 1f;
+            else
+                AnimatorSpeed = 3f;
+        }
+
         private void Update()
         {
             if (Moving)
             {
-                EnemyAnimator.speed = 1;
+                EnemyAnimator.speed = AnimatorSpeed;
                 return;
             }
 
