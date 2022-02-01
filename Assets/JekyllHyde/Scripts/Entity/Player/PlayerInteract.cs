5640f2a2377e1bceb6545f651b0d42cb20866643
Nashira <velocicraft2088@gmail.com>
2022-02-01 01:40:31 -0300

    Add interact capacity for the player

---
+++ Assets/JekyllHyde/Scripts/Entity/Player/PlayerInteract.cs
@@ -0,0 +1,38 @@
+﻿using JekyllHyde.World;
+using System.Collections;
+using UnityEngine;
+
+namespace JekyllHyde.Entity.Player
+{
+    // BLINDADO
+    public class PlayerInteract : MonoBehaviour
+    {
+        private IInteractable InteractiveObject = null;
+        private bool Interacting = false;
+
+        private void FixedUpdate()
+        {
+            if ((Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Return)) && !Interacting && InteractiveObject != null)
+            {
+                Debug.Log("PlayerInteract: Triggering interact...");
+                Interacting = true;
+                InteractiveObject.Interact();
+            }
+            else if (!(Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Return)) && Interacting)
+            {
+                Interacting = false;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            IInteractable interactable = collision.GetComponent<IInteractable>();
+            if (InteractiveObject == null && interactable != null) InteractiveObject = interactable;
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (InteractiveObject == collision.GetComponent<IInteractable>()) InteractiveObject = null;
+        }
+    }
+}
