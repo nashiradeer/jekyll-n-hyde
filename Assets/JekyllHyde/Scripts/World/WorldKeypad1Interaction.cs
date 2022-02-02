7db9987bd769d7fc30bf0fc34a8de4ade9f875d4
Nashira Deer <velocicraft2088@gmail.com>
2022-02-02 12:14:11 -0300

    Add Keypad to Deposit door

---
+++ Assets/JekyllHyde/Scripts/World/WorldKeypad1Interaction.cs
@@ -0,0 +1,42 @@
+﻿using JekyllHyde.Entity.Player;
+using UnityEngine;
+
+namespace JekyllHyde.World
+{
+    public class WorldKeypad1Interaction : MonoBehaviour, IInteractable
+    {
+        [field: SerializeField] private int CurrentWorld { get; set; }
+        [field: SerializeField] private int NewWorld { get; set; }
+        [field: SerializeField] private int InventoryIndex { get; set; }
+        private WorldManager Manager { get; set; }
+        private PlayerInventory Inventory { get; set; }
+
+        public void Interact(PlayerInteract player, bool alternativeKey)
+        {
+            if (alternativeKey)
+            {
+                if (Inventory.Inventory[PlayerInventory.DEPOSIT_LOCK])
+                {
+                    LoadWorld();
+                }
+                else
+                {
+                    player.Keypad1.OnKeyCorrected.AddListener(LoadWorld);
+                    player.Keypad1.Open("GRB");
+                }
+            }
+        }
+
+        private void LoadWorld()
+        {
+            if (InventoryIndex >= 0) Inventory.Inventory[InventoryIndex] = true;
+            Manager.LoadWorld(NewWorld, CurrentWorld);
+        }
+
+        private void Start()
+        {
+            Manager = GameObject.FindGameObjectWithTag("WorldManager").GetComponent<WorldManager>();
+            Inventory = GameObject.FindGameObjectWithTag("PlayerInventory").GetComponent<PlayerInventory>();
+        }
+    }
+}
