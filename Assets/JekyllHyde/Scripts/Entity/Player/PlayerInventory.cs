4014728a5884ac2ccdc539de626d2d7a1524d373
Nashira Deer <velocicraft2088@gmail.com>
2022-02-02 12:11:16 -0300

    Create PlayerInventory to save persistent values

---
+++ Assets/JekyllHyde/Scripts/Entity/Player/PlayerInventory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace JekyllHyde.Entity.Player
+{
+    public class PlayerInventory : MonoBehaviour
+    {
+        public const int DEPOSIT_LOCK = 0;
+
+        public bool[] Inventory { get; private set; } = new bool[1];
+    }
+}
