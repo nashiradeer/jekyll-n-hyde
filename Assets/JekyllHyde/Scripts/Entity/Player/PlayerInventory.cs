fadc9c60ff50ce5a2ab4503ce559e42df8a0802d
Nashira Deer <velocicraft2088@gmail.com>
2022-02-03 11:42:35 -0300

    Add CockloftRoom

--- Assets/JekyllHyde/Scripts/Entity/Player/PlayerInventory.cs
+++ Assets/JekyllHyde/Scripts/Entity/Player/PlayerInventory.cs
@@ -5,7 +5,8 @@ namespace JekyllHyde.Entity.Player
     public class PlayerInventory : MonoBehaviour
     {
         public const int DEPOSIT_LOCK = 0;
+        public const int COCKLOFTROOM_LOCK = 1;
 
-        public bool[] Inventory { get; private set; } = new bool[1];
+        public bool[] Inventory { get; private set; } = new bool[2];
     }
 }
