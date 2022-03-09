2a0e841909e5fdfeda93bb49b2f08be937e5d7da
Nashira Deer <velocicraft2088@gmail.com>
2022-03-08 21:39:27 -0300

    Finalize Splash Scene by now

--- Assets/JekyllHyde/Scripts/UI/Manager/SplashManager.cs
+++ Assets/JekyllHyde/Scripts/UI/Manager/SplashManager.cs
@@ -1,26 +1,43 @@
+using DG.Tweening;
 using NashiraDeer.Splash;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace JekyllHyde.UI.Manager
 {
     public class SplashManager : MonoBehaviour
     {
         [field: SerializeField] private NashiraDeerSplash NashiraDeerSplash { get; set; }
+        [field: SerializeField] private Image HidoiImage { get; set; }
+
+        private AsyncOperation LoadingScene { get; set; }
 
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+
+            LoadingScene = SceneManager.LoadSceneAsync(1);
+            LoadingScene.allowSceneActivation = false;
+
             StartCoroutine(RunSplashes());
         }
 
         private IEnumerator RunSplashes()
         {
-            yield return new WaitForSeconds(2f);
+            HidoiImage.gameObject.SetActive(true);
+            yield return HidoiImage.DOFade(1, 2f).WaitForCompletion();
+            yield return new WaitForSeconds(1.5f);
+            yield return HidoiImage.DOFade(0, 2f).WaitForCompletion();
+            HidoiImage.gameObject.SetActive(false);
+
+            NashiraDeerSplash.gameObject.SetActive(true);
             yield return NashiraDeerSplash.StartSplash();
-            SceneManager.LoadScene(1);
+            NashiraDeerSplash.gameObject.SetActive(false);
+
+            LoadingScene.allowSceneActivation = true;
         }
     }
 }
